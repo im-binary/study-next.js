e608a35d0867d9ac1772c71d6bd8dae2a5191b80
pongdang <pongdang0121@gmail.com>
2022-06-04 14:09:22 +0900

    next.js에서 router, Link 사용하기

---
+++ components/NavBar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <Link href='/'>
+        <a style={{ color: router.pathname === "/" ? "blue" : "red" }}>Home</a>
+      </Link>
+      <Link href='/about'>
+        <a style={{ color: router.pathname === "/about" ? "blue" : "red" }}>About</a>
+      </Link>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Home() {
-  return <h1>Hi!</h1>;
+  return (
+    <div>
+      <NavBar />
+      <h1>Hi!</h1>
+    </div>
+  );
 }
